2de5a516c189cb93e7a6f8cfe789987747856798
skoriop <karthikp.ivy@gmail.com>
2025-11-19 18:54:31 +0530

    feat: finish scene

--- scene/main.cpp
+++ scene/main.cpp
@@ -28,7 +28,7 @@ bool drawEdges = false;
 bool lightingEnabled = true;
 
 // camera
-Camera camera(glm::vec3(0.0f, 10.0f, 30.0f));
+Camera camera(glm::vec3(0.0f, 30.0f, 50.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
